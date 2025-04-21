c0a3001c8fd67a4f1742736574713245f96eae69
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-04-21 14:27:45 +0530

    ..

--- admin_index.php
+++ admin_index.php
@@ -6,19 +6,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stockout India Dashboard</title>
-    <script>
+    <!-- <script>
         const token = localStorage.getItem('authToken');
         const role = localStorage.getItem('role');
         if (!token || role !== 'admin') {
             alert("Admin access only.");
             window.location.href = "login.php";
         }
-  </script>
+    </script> -->
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="uploads/favicon/apple-touch-icon.png">
     <!-- <script src="configs/auth.js"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <!-- <script>
+    <script>
         const token = localStorage.getItem('authToken');
         const role = localStorage.getItem('role');
 
@@ -32,7 +31,8 @@
                 window.location.href = "login.php";
             });
         }
-    </script> -->
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/lucide@latest"></script>
     <link rel="stylesheet" href="custom/admin_style.css">

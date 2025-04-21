def3a6e11def72ad683d891344ab235689252218
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-04-21 22:31:00 +0530

    ..

--- admin/admin_inc/coupons.php
+++ admin/admin_inc/coupons.php
@@ -97,40 +97,6 @@
       });
   }
 
-  // function deleteCoupon(id) {
-  //   const token = localStorage.getItem("authToken");
-
-  //   Swal.fire({
-  //     title: "Are you sure?",
-  //     text: "This will permanently delete the coupon.",
-  //     icon: "warning",
-  //     showCancelButton: true,
-  //     confirmButtonText: "Yes, delete it!",
-  //     cancelButtonText: "Cancel"
-  //   }).then((result) => {
-  //     if (result.isConfirmed) {
-  //       fetch(`${BASE_URL}/coupon/${id}`, {
-  //         method: "DELETE",
-  //         headers: {
-  //           "Authorization": `Bearer ${token}`
-  //         }
-  //       })
-  //         .then(res => res.json())
-  //         .then(data => {
-  //           if (data.success) {
-  //             Swal.fire("Deleted!", "Coupon has been deleted.", "success");
-  //             fetchCoupons();
-  //           } else {
-  //             Swal.fire("Error", data.message || "Failed to delete coupon", "error");
-  //           }
-  //         })
-  //         .catch(error => {
-  //           console.error("Delete error:", error);
-  //           Swal.fire("Error", "Server error while deleting.", "error");
-  //         });
-  //     }
-  //   });
-  // }
   function deleteCoupon(id) {
     const token = localStorage.getItem("authToken");
 
--- admin/admin_index.php
+++ admin/admin_index.php
@@ -7,14 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stockout India Dashboard</title>
     <script src="../configs/auth.js"></script>
-    <!-- <script>
-        const token = localStorage.getItem('authToken');
-        const role = localStorage.getItem('role');
-        if (!token || role !== 'admin') {
-            alert("Admin access only.");
-            window.location.href = "login.php";
-        }
-    </script> -->
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="../uploads/favicon/apple-touch-icon.png">
     <!-- <script src="configs/auth.js"></script> -->
